b3495af94d745936a15328b6a594d8b64a798631
DarMamsalehHTL <d.mamsaleh@students.htl-leonding.ac.at>
2026-05-12 09:15:28 +0200

    styled login and fixed some features

---
+++ excuse-list/test-login.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const data = JSON.stringify({
+  username: 'student',
+  password: '123'
+});
+
+const req = http.request({
+  hostname: 'localhost',
+  port: 3000,
+  path: '/api/login',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}, (res) => {
+  let body = '';
+  res.on('data', chunk => body += chunk);
+  res.on('end', () => console.log('Response:', res.statusCode, body));
+});
+
+req.on('error', e => console.error('Error:', e));
+req.write(data);
+req.end();
